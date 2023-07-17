d5f6db640e92c73bff90431b04ba0d146cf6c3ba
Kye <kye@apacmediasolutions.com>
2023-07-17 12:54:32 -0400

    clean up imagebind path

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'pegasusX',
   packages = find_packages(exclude=[]),
-  version = '0.3.5',
+  version = '0.3.6',
   license='MIT',
   description = 'pegasus - Pytorch',
   author = 'Kye Gomez',
