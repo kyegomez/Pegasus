873810074ef5423811689ca6135962c4d880bcff
Kye <kye@apacmediasolutions.com>
2023-07-17 00:23:12 -0400

    file path errors

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'pegasusX',
   packages = find_packages(exclude=[]),
-  version = '0.1.6',
+  version = '0.1.7',
   license='MIT',
   description = 'pegasus - Pytorch',
   author = 'Kye Gomez',
