de149f29f1e72fa03aab6688b916cc8f87bd4d60
Kye <kye@apacmediasolutions.com>
2023-07-16 23:44:58 -0400

    init

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'pegasusX',
   packages = find_packages(exclude=[]),
-  version = '0.1.0',
+  version = '0.1.1',
   license='MIT',
   description = 'pegasus - Pytorch',
   author = 'Kye Gomez',
